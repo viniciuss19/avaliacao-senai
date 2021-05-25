30247f7d7a3d6188ab23949406f2341c62639984
viniciuss19 <viniterlera@gmail.com>
2021-05-25 18:30:28 -0300

    salvando :p

--- Avaliação/Avaliação/GerenciarCliente.Designer.cs
+++ Avaliação/Avaliação/GerenciarCliente.Designer.cs
@@ -36,10 +36,18 @@ namespace Avaliação
             this.btnEditar = new System.Windows.Forms.Button();
             this.tbNome = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tbEndereço = new System.Windows.Forms.TextBox();
+            this.lbRua = new System.Windows.Forms.Label();
+            this.tbEstado = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.tbTelefone = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbCidade = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbRua = new System.Windows.Forms.TextBox();
+            this.tbNumero = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
             this.SuspendLayout();
@@ -47,9 +55,9 @@ namespace Avaliação
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Avaliação.Properties.Resources.download;
-            this.pictureBox1.Location = new System.Drawing.Point(247, 1);
+            this.pictureBox1.Location = new System.Drawing.Point(239, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(187, 122);
+            this.pictureBox1.Size = new System.Drawing.Size(187, 160);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -57,7 +65,7 @@ namespace Avaliação
             // dgvClientes
             // 
             this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvClientes.Location = new System.Drawing.Point(351, 165);
+            this.dgvClientes.Location = new System.Drawing.Point(353, 171);
             this.dgvClientes.Name = "dgvClientes";
             this.dgvClientes.Size = new System.Drawing.Size(407, 200);
             this.dgvClientes.TabIndex = 1;
@@ -79,13 +87,13 @@ namespace Avaliação
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(455, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 25);
+            this.label2.Size = new System.Drawing.Size(90, 25);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Rouf Rouf";
+            this.label2.Text = "O Editor";
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(182, 311);
+            this.btnEditar.Location = new System.Drawing.Point(116, 352);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(125, 54);
             this.btnEditar.TabIndex = 4;
@@ -95,7 +103,7 @@ namespace Avaliação
             // 
             // tbNome
             // 
-            this.tbNome.Location = new System.Drawing.Point(182, 186);
+            this.tbNome.Location = new System.Drawing.Point(12, 187);
             this.tbNome.Name = "tbNome";
             this.tbNome.Size = new System.Drawing.Size(100, 20);
             this.tbNome.TabIndex = 5;
@@ -103,32 +111,32 @@ namespace Avaliação
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(210, 170);
+            this.label3.Location = new System.Drawing.Point(28, 171);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Nome:";
             // 
-            // label4
+            // lbRua
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(210, 209);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Endereço:";
+            this.lbRua.AutoSize = true;
+            this.lbRua.Location = new System.Drawing.Point(36, 249);
+            this.lbRua.Name = "lbRua";
+            this.lbRua.Size = new System.Drawing.Size(43, 13);
+            this.lbRua.TabIndex = 7;
+            this.lbRua.Text = "Estado:";
             // 
-            // tbEndereço
+            // tbEstado
             // 
-            this.tbEndereço.Location = new System.Drawing.Point(182, 225);
-            this.tbEndereço.Name = "tbEndereço";
-            this.tbEndereço.Size = new System.Drawing.Size(100, 20);
-            this.tbEndereço.TabIndex = 8;
+            this.tbEstado.Location = new System.Drawing.Point(12, 265);
+            this.tbEstado.Name = "tbEstado";
+            this.tbEstado.Size = new System.Drawing.Size(100, 20);
+            this.tbEstado.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(210, 248);
+            this.label5.Location = new System.Drawing.Point(28, 210);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(52, 13);
             this.label5.TabIndex = 9;
@@ -136,10 +144,75 @@ namespace Avaliação
             // 
             // tbTelefone
             // 
-            this.tbTelefone.Location = new System.Drawing.Point(182, 264);
+            this.tbTelefone.Location = new System.Drawing.Point(12, 226);
             this.tbTelefone.Name = "tbTelefone";
             this.tbTelefone.Size = new System.Drawing.Size(100, 20);
             this.tbTelefone.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "ID:";
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(39, 143);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(27, 20);
+            this.tbID.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Rua:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(168, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Cidade";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // tbCidade
+            // 
+            this.tbCidade.Location = new System.Drawing.Point(141, 184);
+            this.tbCidade.Name = "tbCidade";
+            this.tbCidade.Size = new System.Drawing.Size(100, 20);
+            this.tbCidade.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(168, 249);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Número:";
+            // 
+            // tbRua
+            // 
+            this.tbRua.Location = new System.Drawing.Point(141, 226);
+            this.tbRua.Name = "tbRua";
+            this.tbRua.Size = new System.Drawing.Size(100, 20);
+            this.tbRua.TabIndex = 18;
+            // 
+            // tbNumero
+            // 
+            this.tbNumero.Location = new System.Drawing.Point(141, 265);
+            this.tbNumero.Name = "tbNumero";
+            this.tbNumero.Size = new System.Drawing.Size(100, 20);
+            this.tbNumero.TabIndex = 19;
             // 
             // GerenciarCliente
             // 
@@ -147,10 +220,18 @@ namespace Avaliação
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbNumero);
+            this.Controls.Add(this.tbRua);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbCidade);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.tbTelefone);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.tbEndereço);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbEstado);
+            this.Controls.Add(this.lbRua);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.tbNome);
             this.Controls.Add(this.btnEditar);
@@ -177,9 +258,17 @@ namespace Avaliação
         private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.TextBox tbNome;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox tbEndereço;
+        private System.Windows.Forms.Label lbRua;
+        private System.Windows.Forms.TextBox tbEstado;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tbTelefone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbCidade;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbRua;
+        private System.Windows.Forms.TextBox tbNumero;
     }
 }
--- Avaliação/Avaliação/GerenciarCliente.cs
+++ Avaliação/Avaliação/GerenciarCliente.cs
@@ -51,10 +51,7 @@ namespace Avaliação
                 conexao.Close();
             }
         }
-        public void EditarCliente()
-        {
-
-        }
+       
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -63,29 +60,10 @@ namespace Avaliação
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int i = 0;
-            try
-            {
-                int linhasselecionadas = dgvClientes.SelectedCells[0].RowIndex;
-                int idCliente = (int)dgvClientes.Rows[linhasselecionadas].Cells[0].Value;
-                i = BD.EditarCliente(idCliente, new Cliente(tbNome.Text, tbEndereço.Text, tbEndereço.Text));
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show(exception.ToString());
-            }
-            finally
-            {
-                if (i > 0)
-                {
-                    MessageBox.Show("Editado com sucesso ROUF");
-                    tbEndereço.Clear();
-                    tbNome.Clear();
-                    tbTelefone.Clear();
-                    dgvClientes.ClearSelection();
-                }
-                AtualizarClientes();
-            }
+            int id = int.Parse(tbID.Text);
+            EditarCliente(id);
+            
+             
         }
 
         private void dgvClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -95,9 +73,62 @@ namespace Avaliação
                 DataGridViewRow row = this.dgvClientes.Rows[e.RowIndex];
                 tbNome.Text = row.Cells["Nome"].Value.ToString();
                 tbTelefone.Text = row.Cells["Telefone"].Value.ToString();
-                tbEndereço.Text = row.Cells["Rua"].Value.ToString();
+                tbEstado.Text = row.Cells["Estado"].Value.ToString();
+                tbCidade.Text = row.Cells["Cidade"].Value.ToString();
+                tbRua.Text = row.Cells["Rua"].Value.ToString();
+                tbNumero.Text = row.Cells["Número"].Value.ToString();
+
+                tbID.Text = row.Cells["ID"].Value.ToString();
+
 
             }
+
+        }
+        public void EditarCliente(int id)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @"Data Source=DESKTOP-SO3COJV;Initial Catalog=provasenai;Integrated Security=True";
+            SqlCommand sql = new SqlCommand();
+            sql.Connection = conexao;
+
+           
+
+            try
+            {
+                conexao.Open();
+                sql.CommandText = $"UPDATE Clientes SET Nome = @nome, Telefone = @telefone, Rua = @rua, Estado = @estado, Cidade = @cidade, Número = @numero WHERE ID = @id";
+                sql.Parameters.AddWithValue("@nome", tbNome.Text);
+                sql.Parameters.AddWithValue("@telefone", tbTelefone.Text);
+                sql.Parameters.AddWithValue("@rua", tbRua.Text);
+                sql.Parameters.AddWithValue("@cidade", tbCidade.Text);
+                sql.Parameters.AddWithValue("@estado", tbEstado.Text);
+                sql.Parameters.AddWithValue("@numero", tbNumero.Text);
+
+                sql.Parameters.AddWithValue("@id", id);
+               int i = sql.ExecuteNonQuery();
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+            finally
+            {
+                MessageBox.Show("Editado com sucesso!");
+                AtualizarClientes();
+                tbNome.Clear();
+                tbEstado.Clear();
+                tbTelefone.Clear();
+                tbRua.Clear();
+                tbCidade.Clear();
+                tbID.Clear();
+                tbNumero.Clear();
+
+                
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
 
         }
     }
